a61290fb6b44914f81320dae32590ab381c04457
galaxea\runze.ge <runze.ge@galaxea.ai>
2024-11-06 20:59:33 +0800

    update

--- install/share/gennodejs/ros/signal_arm/msg/_index.js
+++ install/share/gennodejs/ros/signal_arm/msg/_index.js
@@ -1,22 +1,22 @@
 
 "use strict";
 
+let status = require('./status.js');
+let control = require('./control.js');
+let control_stamped = require('./control_stamped.js');
 let gripper_joint_command = require('./gripper_joint_command.js');
 let motor_error = require('./motor_error.js');
-let control = require('./control.js');
-let arm_control = require('./arm_control.js');
-let status_stamped = require('./status_stamped.js');
-let control_stamped = require('./control_stamped.js');
 let gripper_position_control = require('./gripper_position_control.js');
-let status = require('./status.js');
+let status_stamped = require('./status_stamped.js');
+let arm_control = require('./arm_control.js');
 
 module.exports = {
+  status: status,
+  control: control,
+  control_stamped: control_stamped,
   gripper_joint_command: gripper_joint_command,
   motor_error: motor_error,
-  control: control,
-  arm_control: arm_control,
-  status_stamped: status_stamped,
-  control_stamped: control_stamped,
   gripper_position_control: gripper_position_control,
-  status: status,
+  status_stamped: status_stamped,
+  arm_control: arm_control,
 };
--- install/share/gennodejs/ros/signal_torso/msg/_index.js
+++ install/share/gennodejs/ros/signal_torso/msg/_index.js
@@ -1,20 +1,20 @@
 
 "use strict";
 
-let torso_status_stamped = require('./torso_status_stamped.js');
-let arm_control = require('./arm_control.js');
-let status_stamped = require('./status_stamped.js');
-let status = require('./status.js');
 let torso_control_stamped = require('./torso_control_stamped.js');
 let torso_control = require('./torso_control.js');
+let status = require('./status.js');
+let torso_status_stamped = require('./torso_status_stamped.js');
 let torso_status = require('./torso_status.js');
+let status_stamped = require('./status_stamped.js');
+let arm_control = require('./arm_control.js');
 
 module.exports = {
-  torso_status_stamped: torso_status_stamped,
-  arm_control: arm_control,
-  status_stamped: status_stamped,
-  status: status,
   torso_control_stamped: torso_control_stamped,
   torso_control: torso_control,
+  status: status,
+  torso_status_stamped: torso_status_stamped,
   torso_status: torso_status,
+  status_stamped: status_stamped,
+  arm_control: arm_control,
 };
